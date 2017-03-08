84cc7cb8c1142cc832c117f8b911c6fc4e876b7e
amanmehta-maniac <amanmehta1997@gmail.com>
2017-03-09 02:10:47 +0530

    new compiled dictionaries

---
+++ apertium-mar-hin/comp.sh
@@ -0,0 +1,7 @@
+lt-comp lr ../apertium-mar/apertium-mar.mar.dix mar-hin.automorf.bin
+lt-comp rl ../apertium-hin/apertium-hin.hin.dix mar-hin.autogen.bin
+lt-comp lr ../apertium-hin/apertium-hin.hin.dix hin-mar.automorf.bin
+lt-comp rl ../apertium-mar/apertium-mar.mar.dix hin-mar.autogen.bin
+lt-comp lr apertium-mar-hin.mar-hin.dix mar-hin.autobil.bin
+lt-comp rl apertium-mar-hin.mar-hin.dix hin-mar.autobil.bin
+lt-comp rl apertium-mar-hin.mar-hin.dix hin-mar.autobil.bin
