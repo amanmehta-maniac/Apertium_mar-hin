3bc69bd0dae5bf5899a5de2cb17aa6ba381cd1c4
amanmehta-maniac <amanmehta1997@gmail.com>
2017-03-12 18:35:39 +0530

    casual update

--- apertium-mar-hin/comp.sh
+++ apertium-mar-hin/comp.sh
@@ -4,4 +4,10 @@ lt-comp lr ../apertium-hin/apertium-hin.hin.dix hin-mar.automorf.bin
 lt-comp rl ../apertium-mar/apertium-mar.mar.dix hin-mar.autogen.bin
 lt-comp lr apertium-mar-hin.mar-hin.dix mar-hin.autobil.bin
 lt-comp rl apertium-mar-hin.mar-hin.dix hin-mar.autobil.bin
-lt-comp rl apertium-mar-hin.mar-hin.dix hin-mar.autobil.bin
+lt-comp lr ../apertium-mar/apertium-mar.mar.dix mar.analyser.bin
+lt-comp lr ../apertium-hin/apertium-hin.hin.dix hin.analyser.bin
+
+mv hin.analyser.bin ../apertium-hin/
+mv mar.analyser.bin ../apertium-mar/
+./autogen.sh --with-lang1=../apertium-mar --with-lang2=../apertium-hin
+make
